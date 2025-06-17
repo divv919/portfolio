eaeb5bbad2ff031dd18e94196b6f82ce4760be95
divv919 <divyanshsoni919@gmail.com>
2025-06-17 13:19:57 +0530

    metadata  remove

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,66 +1,66 @@
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import "../styles/globals.css"
-import { Toaster } from "react-hot-toast"
+// import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../styles/globals.css";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
-})
+});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
-})
+});
 
-export const metadata: Metadata = {
-  icons: {
-    icon: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "./assets/Images/icon/icon-light.png",
-        type: "image/png",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "./assets/Images/icon/icon-dark.png",
-        type: "image/png",
-      },
-    ],
-    shortcut: [
-      {
-        media: "(prefers-color-scheme: light)",
-        url: "./assets/Images/icon/icon-light.png",
-        type: "image/png",
-      },
-      {
-        media: "(prefers-color-scheme: dark)",
-        url: "./assets/Images/icon/icon-dark.png",
-        type: "image/png",
-      },
-    ],
-  },
-  openGraph: {
-    title: "Rinkit Adhana",
-    description: "This is my portfolio website.",
-    url: "https://www.rinkit.tech",
-    siteName: "Switchfolio",
-    images: [
-      {
-        url: "https://res.cloudinary.com/dhcocqegu/image/upload/v1749401017/og_k1rjzx.png",
-        width: 1200,
-        height: 630,
-      },
-    ],
-    locale: "en-IN",
-    type: "website",
-  },
-}
+// export const metadata: Metadata = {
+//   icons: {
+//     icon: [
+//       {
+//         media: "(prefers-color-scheme: light)",
+//         url: "./assets/Images/icon/icon-light.png",
+//         type: "image/png",
+//       },
+//       {
+//         media: "(prefers-color-scheme: dark)",
+//         url: "./assets/Images/icon/icon-dark.png",
+//         type: "image/png",
+//       },
+//     ],
+//     shortcut: [
+//       {
+//         media: "(prefers-color-scheme: light)",
+//         url: "./assets/Images/icon/icon-light.png",
+//         type: "image/png",
+//       },
+//       {
+//         media: "(prefers-color-scheme: dark)",
+//         url: "./assets/Images/icon/icon-dark.png",
+//         type: "image/png",
+//       },
+//     ],
+//   },
+//   openGraph: {
+//     title: "Rinkit Adhana",
+//     description: "This is my portfolio website.",
+//     url: "https://www.rinkit.tech",
+//     siteName: "Switchfolio",
+//     images: [
+//       {
+//         url: "https://res.cloudinary.com/dhcocqegu/image/upload/v1749401017/og_k1rjzx.png",
+//         width: 1200,
+//         height: 630,
+//       },
+//     ],
+//     locale: "en-IN",
+//     type: "website",
+//   },
+// }
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
@@ -71,5 +71,5 @@ export default function RootLayout({
         {children}
       </body>
     </html>
-  )
+  );
 }
